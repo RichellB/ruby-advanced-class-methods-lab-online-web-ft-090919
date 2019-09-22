d2c8ff3b164e3a2b41f2f75407132c536e7dbd3e
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-22 13:31:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
     end
   end
 
-  def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(song_name)
    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
   
