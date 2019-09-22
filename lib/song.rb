4cf6cabe8177d3d787052a0a81b70967f629ed3a
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-22 13:26:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,33 +2,35 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
-  def self.create 
-    @@all << self.new 
-    self
-  end 
+   def save
+    self.class.all << self
+  end
+
+  def self.create
+    songs = Song.new
+    songs.save
+    songs
+  end
   
-  def self.create_by_name(name)
-    @name = name 
-    @all << self 
+  def self.create_by_name(song_name)
+    songs = self.create
+    songs.name = song_name
+    songs
   end
   
   def self.all
     @@all
   end
   
-  def self.new_by_name(name)
-    @name = name
-    self.name 
-  end
-    
-  def save
-    self.class.all << self
+  def self.new_by_name(song_name)
+    songs = self.new 
+    songs.name = song_name 
+    songs
+   
   end
   
   def self.find_or_create_by_name(name)
-    @@all.find do |songs| 
-      if songs.name == name
-        name
+   songs.find
       else 
         @@all << name
       end 
